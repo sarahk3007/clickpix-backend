f4343bf70c233c649b01878f9fc8f489a62b4130
Rebecca <rebecca@mipo.co.il>
2024-02-23 08:30:40 +0200

    add release pixels cron

--- basic/commands/ReleasePixelsController.php
+++ basic/commands/ReleasePixelsController.php
@@ -18,35 +18,45 @@ class ReleasePixelsController extends Controller
 {
     public function actionIndex()
     {
-        $twilio = new TwilioSdk;
-        $response = $twilio->SendSMS('test release', '+972547488988');
+        // $twilio = new TwilioSdk;
+        // $response = $twilio->SendSMS('test release', '+972547488988');
 
-        // $connection = Yii::$app->getDb();
+        $connection = Yii::$app->getDb();
 
-        // $paymentSql = "SELECT * FROM payment_history WHERE start_date IS NOT NULL AND end_date IS NULL";
-        // $command = $connection->createCommand($paymentSql);
-        // $paymentHistories = $command->queryAll();
-        // if (count($paymentHistories) > 0) {
-        //     $secretKey = Yii::$app->params['stripe']['secretKey'];
-        //     \Stripe\Stripe::setApiKey($secretKey);
-        //     foreach ($paymentHistories as $paymentHistory) {
-        //         $sessionId = $paymentHistory['session_id'];
-        //         $session = \Stripe\Checkout\Session::retrieve($sessionId);
-        //         $paymentIntentId = $session->payment_intent;
-        //         $dateTime = strtotime('now');
-        //         if ($dateTime - $paymentHistory['start_date'] >= 50 && !$paymentIntentId) {
-        //             $sql = "UPDATE `image` SET available = 1, paid = false WHERE id IN " . $paymentHistory['ids'];
-        //             $command = $connection->createCommand($sql);
-        //             $result = $command->execute();
-        //             $secondSql = "UPDATE payment_history SET end_date = " . $dateTime . " WHERE end_date IS NULL AND ids = '" . $paymentHistory['ids'] . "' AND session_id = '" . $sessionId . "'";
-        //             $secondCommand = $connection->createCommand($secondSql);
-        //             $secondResult = $secondCommand->execute();
-        //         }
-        //     }
-        // } else {
-        //     return [
-        //         'success' => true,
-        //     ];
-        // }
+        $paymentSql = "SELECT * FROM payment_history WHERE start_date IS NOT NULL AND end_date IS NULL";
+        $command = $connection->createCommand($paymentSql);
+        $paymentHistories = $command->queryAll();
+        if (count($paymentHistories) > 0) {
+            $secretKey = Yii::$app->params['stripe']['secretKey'];
+            \Stripe\Stripe::setApiKey($secretKey);
+            foreach ($paymentHistories as $paymentHistory) {
+                $sessionId = $paymentHistory['session_id'];
+                $dateTime = strtotime('now');
+                try {
+                    $session = \Stripe\Checkout\Session::retrieve($sessionId);
+                    if ($session) {
+                        $paymentIntentId = $session->payment_intent;
+                        $end = $dateTime - $paymentHistory['start_date'] >= 50 && !$paymentIntentId;
+                    } else {
+                        $end = true;
+                    }
+                } catch (\Stripe\Exception\InvalidRequestException $e) {
+                    $end = true;
+                }
+                
+                if ($end) {
+                    $sql = "UPDATE `image` SET available = 1, paid = false WHERE id IN " . $paymentHistory['ids'];
+                    $command = $connection->createCommand($sql);
+                    $result = $command->execute();
+                    $secondSql = "UPDATE payment_history SET end_date = " . $dateTime . " WHERE end_date IS NULL AND ids = '" . $paymentHistory['ids'] . "' AND session_id = '" . $sessionId . "'";
+                    $secondCommand = $connection->createCommand($secondSql);
+                    $secondResult = $secondCommand->execute();
+                }
+            }
+        } else {
+            return [
+                'success' => true,
+            ];
+        }
     }
 }
